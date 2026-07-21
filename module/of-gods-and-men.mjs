96095ef2c58b761cd9eb7b37459ea3e0ebaae17c
Pablomorais121 <pablomoraisalvarez@gmail.com>
2026-07-21 14:14:56 +0200

    feat: created basic god sheets

--- module/of-gods-and-men.mjs
+++ module/of-gods-and-men.mjs
@@ -2,6 +2,7 @@ import AscendedData from "./data/actor-ascended.mjs";
 import GodData from "./data/item-god.mjs";
 import ArchetypeData from "./data/item-archetype.mjs";
 import AscendedSheet from "./sheets/ascended-sheet.mjs";
+import GodSheet from "./sheets/god-sheet.mjs";
 
 Hooks.once("init", () => {
     console.log("Of Gods and Men | Initializing System");
@@ -10,12 +11,18 @@ Hooks.once("init", () => {
     CONFIG.Item.dataModels.god = GodData;
     CONFIG.Item.dataModels.archetype = ArchetypeData;
 
-    const {Actors} = foundry.documents.collections;
+    const {Actors, Items} = foundry.documents.collections;
 
     Actors.registerSheet("of-gods-and-men", AscendedSheet, {
         types: ["ascended"],
         makeDefault: true,
         label: "Ascended Sheet"
+    });
+
+    Items.registerSheet("of-gods-and-men", GodSheet, {
+        types: ["god"],
+        makeDefault: true,
+        label: "God Sheet"
     });
 
 }); 
---
+++ module/sheets/god-sheet.mjs
@@ -0,0 +1,29 @@
+const { HandlebarsApplicationMixin} = foundry.applications.api;
+const { ItemSheetV2 } = foundry.applications.sheets;
+
+export default class GodSheet extends HandlebarsApplicationMixin(ItemSheetV2) {
+    
+    static DEFAULT_OPTIONS ={
+        classes: ["of-gods-and-men", "sheet", "item", "god"],
+        position: {
+            width: 500,
+            height: 600
+        },
+        form: {
+            submitOnChange: true,
+        }
+    };
+
+    static PARTS = {
+        body: {
+            template: "systems/of-gods-and-men/templates/item/god-sheet.hbs"
+        }
+    };
+
+    async _prepareContext(options) {
+        const context = await super._prepareContext(options);
+        context.actor = this.item;
+        context.system = this.item.system;
+        return context;
+    }
+}
