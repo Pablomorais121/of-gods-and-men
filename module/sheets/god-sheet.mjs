8674bf3a56abdda887936f62e74b557b074831ca
Pablomorais121 <pablomoraisalvarez@gmail.com>
2026-07-22 18:48:59 +0200

    feat: added spells inside god sheets

--- module/sheets/god-sheet.mjs
+++ module/sheets/god-sheet.mjs
@@ -5,12 +5,12 @@ export default class GodSheet extends HandlebarsApplicationMixin(ItemSheetV2) {
     
     static DEFAULT_OPTIONS ={
         classes: ["of-gods-and-men", "sheet", "item", "god"],
-        position: {
-            width: 500,
-            height: 600
-        },
-        form: {
-            submitOnChange: true,
+        position: { width: 500, height: 600 },
+        form: { submitOnChange: true },
+        actions: {
+            addSpell: GodSheet.#onAddSpell,
+            deleteSpell: GodSheet.#onDeleteSpell,
+            saveSpell: GodSheet.#onSaveSpell
         }
     };
 
@@ -26,4 +26,29 @@ export default class GodSheet extends HandlebarsApplicationMixin(ItemSheetV2) {
         context.system = this.item.system;
         return context;
     }
+
+    static async #onAddSpell(event, target) {
+        const spell = foundry.utils.deepClone(this.item.system.spells);
+        spell.push({name: "", cost: 0, description: ""});
+        await this.item.update({ "system.spells": spell });
+    }
+
+    static async #onDeleteSpell(event, target) {
+        const index = Number(target.dataset.index);
+        const spells = foundry.utils.deepClone(this.item.system.spells);
+        spells.splice(index, 1);
+        await this.item.update({ "system.spells": spells});
+    }
+
+    static async #onSaveSpell(event, target) {
+        const rows = this.element.querySelectorAll(".spell-row");
+        const spells = Array.from(rows).map(row =>{
+            const cost = Number(row.querySelector(".spell-cost").value);
+            const name = row.querySelector(".spell-name").value;
+            const description = row.querySelector(".spell-description").value;
+            return { cost, name, description };
+        });
+        await this.item.update({ "system.spells": spells});
+    }
+
 }
